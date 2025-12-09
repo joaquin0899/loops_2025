594a302e666e8cfc2efdc9985a01dc451968fb9f
joaquin0899 <jrflores15@cps.edu>
2025-12-09 15:02:20 +0000

    done

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,11 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+
+
+
+
+for x in range(1,21):
+    if x == 13:
+        break
+    else:
+        print(x)
--- 1_loops.py
+++ 1_loops.py
@@ -13,8 +13,28 @@ for fruit in fruits:
 
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
-for subject in subjects:
-    print(subject)
+# for subject in subjects:
+#     print(subject)
+# #print out each subject but stop when you hit history
+
+# for x in subjects:
+#     if x == "History":
+#         break
+#     else:
+#         print(x)
+
+# for x in subjects:
+#     if x == "Science":
+#         continue
+#     else:
+#         print(x)
+
+for i in range(len(subjects)):
+    print(f"Subject {i}: {subjects[1]}")
+
+
+
+
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
@@ -25,3 +45,9 @@ numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+
+
+total = 0 
+for numbers in numbers:
+    total += numbers
+print(total)
