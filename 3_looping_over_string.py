ac4bd97697222f7bca69a859f0b30b3988612a0e
joaquin0899 <jrflores15@cps.edu>
2025-12-11 14:28:49 +0000

    push

--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -4,3 +4,31 @@
 
 # Challenge:
 # Count how many vowels are in the word.
+
+
+
+
+word = input("Say any random word: ")
+
+for char in word:
+    print(char)
+
+
+# initializing count variable
+count = 0
+
+# declaring a variable for index
+i = 0
+
+
+for i in range(len(word)):
+    if (
+        (word[i] == "a")
+        or (word[i] == "e")
+        or (word[i] == "i")
+        or (word[i] == "o")
+        or (word[i] == "u")
+    ):
+        count += 1
+
+print("Number of vowels in the given string is: ", count)
